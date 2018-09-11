9d407d0f085a94f5e9f35fffff6912faee4450ce
Josh Nowak <Joshnwk@gmail.com>
2018-09-11 11:38:51 -0600

    Added r function to show structure and documentation.  This should be deleted at some future date

---
+++ R/foo.R
@@ -0,0 +1,14 @@
+#' Fake function to show structure of comments and code
+#'
+#' @param x numeric
+#'
+#' @return numeric of length x
+#' @export
+#'
+#' @examples
+#' foo(4)
+foo <- function(x){
+  out <- x + 2
+
+return(out)
+}
